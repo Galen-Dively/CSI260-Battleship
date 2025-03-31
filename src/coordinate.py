405f0e8d42244f159cb793417640b24f18e6f529
Galen-Dively <65838051+Galen-Dively@users.noreply.github.com>
2025-03-31 15:07:01 -0400

    Inital Commit
    Added neccasry files

---
+++ src/coordinate.py
@@ -0,0 +1,7 @@
+
+
+from typing import NamedTuple
+
+
+class Coordinate(NamedTuple):
+    pass
---
+++ src/ship.py
@@ -0,0 +1,11 @@
+
+class Ship:
+    def __init__(self):
+        pass
+
+
+    def is_hit(self, coordinate) -> bool:
+        pass
+
+    def __str__(self):
+        pass
