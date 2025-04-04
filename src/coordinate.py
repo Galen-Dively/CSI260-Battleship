55728f5466bf5d30a3e29902e287d1a2521f5cba
abbi gehlbach <abigail.gehlbach@mymail.champlain.edu>
2025-04-04 11:07:41 -0400

    base of coord class and upating board to use new coord system

--- src/coordinate.py
+++ src/coordinate.py
@@ -1,7 +1,24 @@
 
-from dataclasses import dataclass
-
-
-@dataclass
 class Coordinate:
-    pass
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+    @classmethod
+    def from_string(cls, s: str):
+        """returns coord obj from string input 'A3 -> (0,2)'"""
+        col_letter = s[0].upper()
+        row_number = int(s[1:]) - 1
+        col_number = ord(col_letter) - ord('A')
+        return cls(row_number, col_number)
+
+    def __str__(self):
+        """to string method, takes coord obj and prints string rpresenetation (2,3)->'C4'"""
+        col_letter = chr(self.col + ord('A'))
+        return f"{col_letter}{self.row + 1}"
+
+    def __eq__(self, other):
+        """checks if two coords are equal"""
+        if isinstance(other, Coordinate):
+            return self.row == other.row and self.col == other.col
+        return False
